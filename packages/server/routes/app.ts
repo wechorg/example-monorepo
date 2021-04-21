43212c6fc3db02b8a74dd47099baf2b07498bb8b
Wechuli <wechulipaul@yahoo.com>
2021-04-21 16:20:12 +0300

    stuff

--- packages/server/routes/app.ts
+++ packages/server/routes/app.ts
@@ -1,5 +1,2 @@
-function shout() {
-  console.log("shout");
-}
-
-shout();
+#!/usr/bin/env node --trace-warnings --loader ts-node/esm --experimental-specifier-resolution=node --unhandled-rejections=strict
+console.log("hi");
